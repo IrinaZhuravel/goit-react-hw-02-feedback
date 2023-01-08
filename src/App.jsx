f85805a9180b225c62fcc823782ce03a5e45a423
IrinaZhuravel <90681450+IrinaZhuravel@users.noreply.github.com>
2023-01-08 18:02:22 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+    </div>
+  );
+};
+
